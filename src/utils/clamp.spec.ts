9ca67e5a74c0656eba712f014670e58ac73820f3
s-r-x <strax1990@gmail.com>
2021-08-31 10:17:13 +0500

    test: clamp util

---
+++ src/utils/clamp.spec.ts
@@ -0,0 +1,8 @@
+import { clamp } from './clamp';
+
+test('clamp util', () => {
+  expect(clamp(1, 1, 10)).toEqual(1);
+  expect(clamp(10, 1, 10)).toEqual(10);
+  expect(clamp(0, 1, 10)).toEqual(1);
+  expect(clamp(11, 1, 10)).toEqual(10);
+});
