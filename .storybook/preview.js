8bfcc1c23e4454add910e7540d9b9fa08c389c43
s-r-x <strax1990@gmail.com>
2021-09-20 20:49:52 +0500

    chore: remove transformSource from storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,20 +1,18 @@
-import prettier from 'prettier/standalone';
-import prettierBabel from 'prettier/parser-babel';
-
+// import prettier from 'prettier/standalone';
+// import prettierBabel from 'prettier/parser-babel';
+//
 export const parameters = {
   layout: 'centered',
   options: {
     storySort: {
-      method: '',
       order: ['Playground', 'Styling', 'Tooltips'],
-      locales: '',
     },
   },
-  docs: {
-    transformSource: input =>
-      prettier.format(input, {
-        parser: 'babel',
-        plugins: [prettierBabel],
-      }),
-  },
+  // docs: {
+  //   transformSource: input =>
+  //     prettier.format(input, {
+  //       parser: 'babel',
+  //       plugins: [prettierBabel],
+  //     }),
+  // },
 };
