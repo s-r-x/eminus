945caa8dbcbbc7e63e810c80cccb710a83b89b4f
s-r-x <strax1990@gmail.com>
2021-09-20 15:12:31 +0500

    docs: custom tooltip renderer story

--- src/stories/Eminus.stories.tsx
+++ src/stories/Eminus.stories.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import Eminus from '..';
+import './global.less';
 
 export default {
-  title: 'Eminus',
+  title: 'Playground',
   component: Eminus,
 } as ComponentMeta<typeof Eminus>;
 
@@ -12,14 +13,7 @@ const Template: ComponentStory<typeof Eminus> = args => {
   useEffect(() => {
     setValue(args.value);
   }, [args.value]);
-  return (
-    <Eminus
-      {...args}
-      style={args.vertical ? undefined : { width: '500px' }}
-      value={value}
-      onChange={setValue}
-    />
-  );
+  return <Eminus {...args} value={value} onChange={setValue} />;
 };
 
 export const Playground = Template.bind({});
