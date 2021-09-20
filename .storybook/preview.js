ca4cbfba045cd6a5cc6b7794480fe79753dc0583
s-r-x <strax1990@gmail.com>
2021-09-20 15:12:13 +0500

    chore: storybook docs formatting

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,13 @@
+import prettier from 'prettier/standalone';
+import prettierBabel from 'prettier/parser-babel';
+
 export const parameters = {
   layout: 'centered',
+  docs: {
+    transformSource: input =>
+      prettier.format(input, {
+        parser: 'babel',
+        plugins: [prettierBabel],
+      }),
+  },
 };
