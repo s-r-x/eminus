0d3323e2ae2625f5444cb2bf281c2cbc02510afc
s-r-x <strax1990@gmail.com>
2021-09-21 12:50:35 +0500

    chore: change base url in vite storybook prod config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,13 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "core": {
-    "builder": "storybook-builder-vite"
-  }
-}
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  async viteFinal(config, { configType }) {
+    if (configType === 'PRODUCTION') {
+      config.base = '/eminus/';
+    }
+    return config;
+  },
+  core: {
+    builder: 'storybook-builder-vite',
+  },
+};
