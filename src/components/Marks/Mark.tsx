2949d4d59accf4e72b6766ab31c1af00f8ca374a
s-r-x <strax1990@gmail.com>
2021-10-08 14:59:15 +0500

    refactor: convert Mark from fc to class

--- src/components/Marks/Mark.tsx
+++ src/components/Marks/Mark.tsx
@@ -1,4 +1,4 @@
-import React, { memo, CSSProperties } from 'react';
+import React from 'react';
 import type {
   Mark as TMark,
   MarkLabelFormatter,
@@ -16,33 +16,48 @@ type Props = {
   onMouseDown: (value: number) => void;
   labelFormatter?: MarkLabelFormatter;
 };
-const Mark = (props: Props) => {
-  const value = typeof props.mark === 'number' ? props.mark : props.mark.value;
-  const x = mapNumberToPercent(value, props.min, props.max);
-  const sharedStyle: CSSProperties = {
-    [props.vertical ? 'top' : 'left']: clamp(x, 0, 100) + '%',
-  };
-  const onMouseDown = (e: React.MouseEvent | React.TouchEvent) => {
+
+class Mark extends React.PureComponent<Props> {
+  onMouseDown = (e: React.MouseEvent | React.TouchEvent) => {
     e.preventDefault();
     e.stopPropagation();
-    props.onMouseDown(value);
+    this.props.onMouseDown(this.value);
   };
-  const label = props.labelFormatter
-    ? props.labelFormatter(props.mark)
-    : typeof props.mark === 'number'
-    ? props.mark
-    : props.mark.label || props.mark.value;
-  return (
-    <div
-      className={clsx('Eminus-mark', props.isActive && 'Eminus-mark--active')}
-      onTouchStart={onMouseDown}
-      onMouseDown={onMouseDown}
-    >
-      <div style={sharedStyle} className="Eminus-mark-value" />
-      <div style={sharedStyle} className="Eminus-mark-label">
-        {label}
+  get label(): React.ReactElement | string | number {
+    return this.props.labelFormatter
+      ? this.props.labelFormatter(this.props.mark)
+      : typeof this.props.mark === 'number'
+      ? this.props.mark
+      : this.props.mark.label || this.props.mark.value;
+  }
+  get value(): number {
+    return typeof this.props.mark === 'number'
+      ? this.props.mark
+      : this.props.mark.value;
+  }
+  get sharedStyle(): React.CSSProperties {
+    const x = mapNumberToPercent(this.value, this.props.min, this.props.max);
+    return {
+      [this.props.vertical ? 'top' : 'left']: clamp(x, 0, 100) + '%',
+    };
+  }
+  render() {
+    const { sharedStyle } = this;
+    return (
+      <div
+        className={clsx(
+          'Eminus-mark',
+          this.props.isActive && 'Eminus-mark--active'
+        )}
+        onTouchStart={this.onMouseDown}
+        onMouseDown={this.onMouseDown}
+      >
+        <div style={sharedStyle} className="Eminus-mark-value" />
+        <div style={sharedStyle} className="Eminus-mark-label">
+          {this.label}
+        </div>
       </div>
-    </div>
-  );
-};
-export default memo(Mark);
+    );
+  }
+}
+export default Mark;
